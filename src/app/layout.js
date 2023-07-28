727eb7ddd050dc4d3d52cc92ffae46755c1e4854
AlissonRuiz18 <98785962+AlissonRuiz18@users.noreply.github.com>
2023-07-28 15:52:34 -0600

    subido proyecto

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+
+import "./globals.css";
+import { Inter, Roboto, Poppins } from "next/font/google";
+import { ThemeProvider } from "../context/ThemeContext";
+import AuthProvider from "../components/AuthProvider/AuthProvider";
+
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Alisson Ruiz",
+  description: "This is the description",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <AuthProvider>
+            <div className="container">
+             <Navbar/>
+              {children}
+            <Footer/>
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
